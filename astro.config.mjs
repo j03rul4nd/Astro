620068f0e294cd9b41664b15cf979c4c8289c6ff
joel <joelbenitezdonari@gmail.com>
2023-01-27 21:05:28 +0100

    github v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://j03rul4nd.github.io',
-  base: '/Astro',
+  base: '/Astro-Joel',
 });
